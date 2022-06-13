dc0f358fb41da4f0f9136b0f80f8f356c605c5ff
gkum4 <gustavokumasawa@gmail.com>
2022-06-13 18:31:11 -0300

    Move index.js to src

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({}));
-app.use(express.json())
+app.use(express.json());
 
 app.listen(3333, () => {
   console.log('⚡️ Server started on port 3333!');
