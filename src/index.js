1f9441407b23ccd44db8dadac83e3d4c2a908455
gkum4 <gustavokumasawa@gmail.com>
2022-06-15 08:29:01 -0300

    Feat application index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
+
+require('./utils/database');
+const routes = require('./routes');
 
 const app = express();
 
 app.use(cors({}));
 app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('⚡️ Server started on port 3333!');
